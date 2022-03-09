a5bb59f21e1b7bacc3a16f46208af72e40058120
Michal Korejwo <korejwo.michal@gmail.com>
2022-03-09 22:44:18 +0100

    args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::{env};
+
 fn main() {
-    println!("Hello, world!");
+	let args: Vec<String> = env::args().collect();
+
+	if args.len() != 3 {
+		println!("usage: mitm <client_addr> <server_addr>");
+		return;
+	}
 }
