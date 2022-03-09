d9069b11a6d73b68cbcfd1fc04337ca96410b10b
Michal Korejwo <korejwo.michal@gmail.com>
2022-03-09 22:47:34 +0100

    server implements

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 use std::{env};
 
+struct Server {
+	client: String,
+	server: String,
+}
+
+trait IServer {
+	fn new(client: String, server: String) -> Server;
+	fn run(&self);
+}
+
+impl IServer for Server {
+	fn new(client: String, server: String) -> Server {
+		Server { client, server }
+	}
+
+	fn run(&self) {
+		let (client, server) = (self.client.clone(), self.server.clone());
+	}
+}
+
 fn main() {
 	let args: Vec<String> = env::args().collect();
 
@@ -7,4 +27,6 @@ fn main() {
 		println!("usage: mitm <client_addr> <server_addr>");
 		return;
 	}
+
+	Server::new(args[1].to_string(), args[2].to_string()).run();
 }
